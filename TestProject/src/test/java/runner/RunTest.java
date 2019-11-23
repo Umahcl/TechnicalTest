92aa6ed6f23ca3b0747f75e6523ebeeaea023359
admin <admin@admin-PC>
2019-11-23 20:52:50 +0530

    Updated RunnerFile with Json format

--- TestProject/src/test/java/runner/RunTest.java
+++ TestProject/src/test/java/runner/RunTest.java
@@ -6,7 +6,9 @@ import io.cucumber.testng.CucumberOptions;
 //To integrate cucumber with TestNG
 
 @CucumberOptions(features = "./src/test/java/Feature/Purchase.feature", plugin = { "pretty",
-		"html:target" }, strict = false, monochrome = true, glue = "steps")
+		"html:target/CucumberReports",
+		"json:target/cucumber-report.json" }, strict = false, monochrome = true, glue = "steps")
+
 public class RunTest extends AbstractTestNGCucumberTests {
 
 }
